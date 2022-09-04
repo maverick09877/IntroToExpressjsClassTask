70eb8d2314055b92dd81b9efb9057ab9cc7d0a85
Sathwic <sathwicmc97@gmail.com>
2022-09-04 13:22:06 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import React, { useRef } from "react";
 
 function App() {
+  let displayResponse = useRef();
+  let animeIndexInput = useRef();
+  // let animeValueInput = useRef();
+  // let animeDisplay = useRef();
+
+  let getDataFromServer = async (source) => {
+    let reqOptions = {
+      method: "GET",
+    };
+
+    let rawData = await fetch(`http://localhost:9797/${source}`, reqOptions);
+
+    let convertedData = await rawData.json();
+
+    displayResponse.current.innerHTML = convertedData;
+  };
+
+  let getDataForAnime = async () => {
+    let reqOptions = {
+      method: "GET",
+    };
+    // let display=animeDisplay.current.innerHTML;
+    let index = animeIndexInput.current.value;
+    // let value = animeValueInput.current.value;
+
+    let rawData = await fetch(`http://localhost:9797/anime`, reqOptions);
+
+    let convertedData = await rawData.json();
+    console.log(convertedData);
+
+    displayResponse.current.innerHTML = convertedData[index].anime;
+  };
+
+  let serverTimer = () => {
+    setTimeout(() => {
+      displayResponse.current.innerHTML = `Establishing connection to Server....`;
+    }, 3000);
+    setTimeout(() => {
+      displayResponse.current.innerHTML = `Connection established....`;
+    }, 6000);
+    setTimeout(() => {
+      displayResponse.current.innerHTML = `Result will be displayed here.`;
+    }, 9000);
+  };
+  serverTimer();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="buttonContainer">
+        <button
+          type="button"
+          onClick={() => {
+            getDataFromServer("planets");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          PLANETS
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            getDataFromServer("weekdays");
+          }}
+        >
+          WEEKDAYS
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            getDataFromServer("months");
+          }}
+        >
+          MONTHS
+        </button>
+      </div>
+      <div className="animeContainer">
+        <input
+          type="number"
+          placeholder="Enter an index number (0-5)"
+          ref={animeIndexInput}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            getDataForAnime();
+          }}
+        >
+          ANIME
+        </button>
+      </div>
+
+      <h1 ref={displayResponse}></h1>
     </div>
   );
 }
